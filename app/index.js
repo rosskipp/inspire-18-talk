8498acf323fbd2525fae1c7be5dc97d292fbcbdc
Ross Kippenbrock <rkippenbrock@gmail.com>
2018-06-11 12:09:32 -0600

    cleanup node app
    Former-commit-id: a468469172f9839502c95e732742ff7b91b1ddab

--- app/index.js
+++ app/index.js
@@ -5,21 +5,21 @@ const request = require('request');
 const path = require('path');
 
 const predict = (data, done) => {
-    const body = data;
-    const url = 'http://localhost/ross/models/ShotPredictor/predict';
-    const options = {
-        url: url,
-        method: 'post',
-        body: body,
-        json: true,
-        auth: {
-            user: 'ross',
-            pass: 'd580d451-06b9-4c10-a73f-523adca5f48c'
-        }
-    };
-    request(options, (err, res, body) => {
-        done(err, body);
-    });
+  const body = data;
+  const url = 'http://localhost/ross/models/ShotPredictor/predict';
+  const options = {
+    url: url,
+    method: 'post',
+    body: body,
+    json: true,
+    auth: {
+      user: 'USER',
+      pass: 'API_KEY'
+    }
+  };
+  request(options, (err, res, body) => {
+    done(err, body);
+  });
 }
 
 const app = express();
@@ -29,20 +29,18 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.post('/predict', (req, res) => {
-    console.log(req.body);
-    predict(req.body, (err, response) => {
-        console.log(response);
-        res.json({
-            status: "OK",
-            prediction: response.result.shot_prob,
-        });
+  predict(req.body, (err, response) => {
+    res.json({
+      status: "OK",
+      prediction: response.result.shot_prob,
     });
+  });
 });
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 app.listen(process.env.PORT || 8787, () => console.log('Running on port 8787!'));
