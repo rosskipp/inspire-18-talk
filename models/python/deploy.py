28f8d6aae6bdddf873941765ba65d76f144f1655
Ross Kippenbrock <rkippenbrock@gmail.com>
2018-06-11 14:31:38 -0600

    deploy script updates

--- models/python/deploy.py
+++ models/python/deploy.py
@@ -3,33 +3,39 @@ import pickle
 import pandas as pd
 from sklearn.externals import joblib
 
-
-shotLabels = joblib.load('./objects/shot_labels.pkl')
+# Load the pickle file with the model training metrics for meta data
 metrics = joblib.load('./objects/model_metrics.pkl')
-
-def convertShotTypeToInt(shotString):
-    return shotLabels[shotString]
 
 # load in our saved shot model
 MODEL = joblib.load('./objects/log_reg_model.pkl')
 
+# Load the shot labels for the categorical variables
+shotLabels = joblib.load('./objects/shot_labels.pkl')
+
+# Function to convert the shot type string to an int for scoring
+def convertShotTypeToInt(shotString):
+    return shotLabels[shotString]
+
 # instanciate the Promote class with our API information
-USERNAME = "ross"
-API_KEY = "d580d451-06b9-4c10-a73f-523adca5f48c"
-PROMOTE_URL = "http://localhost:3000"
+USERNAME = "USERNAME"
+API_KEY = "API_KEY"
+PROMOTE_URL = "PROMOTE_URL"
 
 p = promote.Promote(USERNAME, API_KEY, PROMOTE_URL)
 
 def shotPredictor(data):
+    # Need to convert the shot type, which comes in as a string, to an int
     data['shotTypeCode'] = convertShotTypeToInt(data['shotType'])
+    # Make sure we have a dataframe, and the order of the columns is right
     df = pd.DataFrame.from_records([data])
     df = df[["shotTypeCode", "shotOnEmptyNet", "shotRush", "shotRebound",
                    "shotDistance", "shotAngleAdjusted"]]
+    # Run the prediction and return the goal probability
     prediction = MODEL.predict_proba(df)
     return {"shot_prob": prediction[0][1]}
 
 
-# Add two flowers as test data
+# Test the model
 TESTDATA = {"shotType": "SNAP", "shotOnEmptyNet": 0, "shotRebound": 0,
             "shotRush": 0, "shotDistance": 7.071068, "shotAngleAdjusted": 45.0}
 print(shotPredictor(TESTDATA))
